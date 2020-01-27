8bc002225d5a4ce0c7df337ea38d1aaba33959ad
canislupus11 <canislupus11@gmail.com>
2020-01-27 23:42:27 +0100

    Update meter.ino

--- meter.ino
+++ meter.ino
@@ -6,7 +6,7 @@ Encoder encoder(A2, 3);
 int mode_button=2;
 float pwr=0;
 int pwr_adc = 0;
-unsigned int att=0;
+int att=0;
 int mode=0; // 0 for set frequency and 1 for set attenuator
 int freq_sel=0;
 char *freq[10]={"30 MHz","50 MHz","144MHz","433MHz","1.2GHz","1.8GHz","2.1GHz","2.4GHz","3.4GHz","5.7GHz"};
@@ -120,7 +120,7 @@ void loop() {
 
   if (mode == 0){
     freq_sel = (encoder.read()/4);
-    if (freq_sel > 10){
+    if (freq_sel > 9){
       encoder.write(0);
       freq_sel = 0;
     }
@@ -131,5 +131,8 @@ void loop() {
   }
   else if ( mode == 1){
     att = (encoder.read()/4);
+    if (att < 0){
+      att = 0;
+    }
   }
 }
