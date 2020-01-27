3e40506de9cc0936f5ae4ec8c613fb7df7b7b59e
canislupus11 <canislupus11@gmail.com>
2020-01-27 21:38:24 +0100

    Update meter.ino

--- meter.ino
+++ meter.ino
@@ -13,20 +13,20 @@ int mode=0; // 0 for set frequency and 1 for set attenuator
 int freq_sel=0;
 char *freq[10]={"30 MHz","50 MHz","144MHz","433MHz","1.2GHz","1.8GHz","2.1GHz","2.4GHz","3.4GHz","5.7GHz"};
 int matrix[10][4] = {
-  {0,1023,0,50},// for 30MHz
-  {0,1023,0,50},// for 50MHz
-  {0,1023,0,50},// for 144MHz
-  {0,1023,0,50},// for 433MHz
-  {0,1023,0,50},// for 1,2GHz
-  {0,1023,0,50},// for 1,8GHz
-  {0,1023,0,50},// for 2,1GHz
-  {0,1023,0,50},// for 2,4GHz
-  {0,1023,0,50},// for 3,4GHz
-  {0,1023,0,50},// for 5,7GHz
+  {0,1023,-30,15},// for 30MHz
+  {0,1023,-30,15},// for 50MHz
+  {0,1023,-30,15},// for 144MHz
+  {0,1023,-30,15},// for 433MHz
+  {0,1023,-30,15},// for 1,2GHz
+  {0,1023,-30,15},// for 1,8GHz
+  {0,1023,-30,15},// for 2,1GHz
+  {0,1023,-30,15},// for 2,4GHz
+  {0,1023,-30,15},// for 3,4GHz
+  {0,1023,-30,15},// for 5,7GHz
 };
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(19200);
   pinMode(mode_button, INPUT_PULLUP);
   lcd.init(); 
   lcd.backlight();
@@ -59,7 +59,7 @@ void mode_interrupt()
 }
 
 void update_disp(){
-  String pwr_s = String(pwr);
+  String pwr_s = String(pwr,3);
   float pwr_w = pow(10.0,(pwr)/10.0);
   lcd.clear();
   lcd.setCursor(1,0);
@@ -76,12 +76,12 @@ void update_disp(){
   lcd.print("Att:");
   lcd.print(att);
   lcd.setCursor(0,1);
-  lcd.print("P:");
+
   lcd.print(pwr_s[0]);
   lcd.print(pwr_s[1]);
   lcd.print(pwr_s[2]);
   lcd.print(pwr_s[3]);
-//  lcd.print(pwr_s[4]);
+  lcd.print(pwr_s[4]);
   lcd.print("dBm ");
 
   if (pwr_w > 999 ){
@@ -95,20 +95,22 @@ void update_disp(){
     lcd.print("W");
   }
   else {
-    String pwr_w_s = String(pwr_w);  
+    String pwr_w_s = String(pwr_w,3);  
     lcd.print(pwr_w_s[0]);
     lcd.print(pwr_w_s[1]);
     lcd.print(pwr_w_s[2]);
     lcd.print(pwr_w_s[3]);
+    lcd.print(pwr_w_s[4]);
     lcd.print("mW");
   }
-  Serial.println(pwr);
+  Serial.print(pwr);
+  Serial.print(";");
   Serial.println(pwr_w);
 }
 
 int measure(){
   float pwr1=0;
-  pwr_adc =  analogRead(A5);
+  pwr_adc =  analogRead(A6);
   pwr1 = map(pwr_adc, matrix[freq_sel][0], matrix[freq_sel][1], matrix[freq_sel][2], matrix[freq_sel][3]);
   return pwr1;
 }
@@ -135,11 +137,5 @@ void loop() {
   else if ( mode == 1){
     att = encoder.getPosition();
   }
-  delay(100);
+  delay(300);
 }
-
-
-
-
-
-
